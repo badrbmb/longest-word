dfb77ead6804f218d8402165f0e00eed31800d81
badr ben mbarek <badr.benb@gmail.com>
2022-12-20 11:07:32 +0000

    TDD: checj attemps in real word

--- tests/test_game.py
+++ tests/test_game.py
@@ -35,3 +35,10 @@ class TestGame:
         # verifiy
         test_word = 'POETRY'
         assert game.is_valid(test_word) is False
+
+
+    def test_unknown_word_is_invalid(self):
+        """A word that is not in the english directory should no be valid"""
+        new_game = Game()
+        new_game.grid = list('KWIENFUQW') # Force the grid to a test case:
+        assert new_game.is_valid('FEUN') is False
