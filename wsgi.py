0745201457b66da5036a230f8805c8c2fb31fe91
badr ben mbarek <badr.benb@gmail.com>
2022-12-20 15:31:35 +0000

    add score

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+from flask_session import Session
 from longest_word.game import Game
 
 app = Flask(__name__)
+# add configuration
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 @app.route('/')
 def home():
@@ -15,6 +20,14 @@ def check():
     game.grid = list(request.form['grid'])
     word = request.form['word']
     is_valid = game.is_valid(word)
+    # init. score if missing
+    if 'score' not in session:
+        # init session score
+        session['score'] = 0
+    if is_valid:
+        # increment global score
+        session['score'] += 1
     return render_template(
-        'check.html', is_valid=is_valid, grid=game.grid, word=word
+        'check.html', is_valid=is_valid, grid=game.grid,
+        word=word, score=session['score']
         )
